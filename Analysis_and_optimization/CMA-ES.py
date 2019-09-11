fix bugs in dihedral angle calculation

Analysis_and_optimization/CMA-ES.py
@@ -3,7 +3,9 @@ from scipy.special import gamma
 import pickle
 import json
 from Update_functions import *
-
+import matplotlib
+matplotlib.use('Agg')
+import matplotlib.pyplot as plt
 def load_json(file):
 	f = open(file,'r')
 	parsed_json = json.load(f)
@@ -12,9 +14,9 @@ def load_json(file):
 	return parsed_json
 
 Ndim = 2
-with open('../iteration_data.pickle','rb') as f:
+with open('../../iteration_data.pickle','rb') as f:
 	iteration_data = pickle.load(f)
-with open('../optimization.pickle','rb') as f:
+with open('../../optimization.pickle','rb') as f:
 	opt = pickle.load(f)
 A = iteration_data[-1]['A']
 kappa = iteration_data[-1]['kappa']
@@ -24,20 +26,25 @@ for i in range(len(A)):
 	num_partners = 0
 	for j in range(1,4):
 		load_dir = "Run_%d_%d"%(i+1,j)
-		json_file = "%s/%s.json"%(load_dir,load_dir)	
+		json_file = "../%s/%s.json"%(load_dir,load_dir)	
 		params = load_json(json_file)
 		dipole_params = params['dipole']
 		assert ((np.isclose(A[i],dipole_params['eps'])) and (np.isclose(kappa[i],dipole_params['kappa'])))
-		dihedral_data = np.loadtxt('%s/dihedral_angle.txt'%(load_dir))
+		dihedral_data = np.loadtxt('../%s/dihedral_angle.txt'%(load_dir))
 		if (np.all(np.isnan(dihedral_data))):
 			fitness[i] = np.inf
 			break;
 		else:
-			sum += 60 - np.mean(dihedral_data)#[int(len(dihedral_data)/2),:])
+			sum += 60 - np.mean(dihedral_data[int(len(dihedral_data)/2):])
 			num_partners += 1
 	assert num_partners is 3
 	fitness[i] = sum/num_partners
-
+np.savetxt('fitness.txt',fitness,fmt='%.6f')
+f = plt.figure()
+plt.plot(np.arange(1,len(A)+1),fitness,'.-')
+plt.xlabel('Candidate')
+plt.ylabel('fitness')
+f.savefig("fitness.pdf", bbox_inches='tight')
 iteration_data[-1]['fitness'] = fitness
 
 Ngen = len(A)
@@ -56,7 +63,7 @@ B = opt[-1]['B']
 C = np.asarray(opt[-1]['C'])
 pc = opt[-1]['pc']
 ps = opt[-1]['ps']
-Z = opt[-1]['Z']
+#Z = opt[-1]['Z']
 z_offspring = opt[-1]['z_offspring']
 num_evals = 10*len(opt)
 
@@ -77,23 +84,17 @@ if (not np.allclose(C,C.T)):
 	print ("Covariance matrix is not symmetric up to numeric precision, enforcing it.....")
 	C = np.triu(C) + np.triu(C,1).T
 evals,B = np.linalg.eigh(C)
-sort_idx = np.argsort(-evals)
-D = np.diag(np.sqrt(evals[sort_idx]))
-B = B[:,sort_idx]
+D = np.diag(np.sqrt(evals))
 BD = np.dot(B,D)
 
 z_offspring = np.random.multivariate_normal(np.zeros(Ndim),np.eye(Ndim),Ngen).T
 x_offspring = x_mean[:,np.newaxis] + sigma*BD@z_offspring
-while (np.any(x_offspring[1,:] < 0.042)):
-    print ("re-sampling....")
-    z_offspring = np.random.multivariate_normal(np.zeros(Ndim),np.eye(Ndim),Ngen).T
-    x_offspring = x_mean[:,np.newaxis] + sigma*BD@z_offspring
 
 temp_iter_dict = dict()
 temp_iter_dict['A'] = x_offspring[0,:]
 temp_iter_dict['kappa'] = x_offspring[1,:]
 iteration_data.append(temp_iter_dict)
-with open('../iteration_data.pickle','wb') as f:
+with open('../../iteration_data.pickle','wb') as f:
 	pickle.dump(iteration_data,f)
 
 temp_opt_dict = dict()
@@ -109,7 +110,7 @@ temp_opt_dict['z_offspring'] = z_offspring
 temp_opt_dict['xmean'] = x_mean
 temp_opt_dict['zmean'] = z_mean
 opt.append(temp_opt_dict)
-with open('../optimization.pickle','wb') as f:
+with open('../../optimization.pickle','wb') as f:
 	pickle.dump(opt,f)
 
 

Analysis_and_optimization/Update_functions.py
@@ -1,9 +1,7 @@
 import numpy as np
 
 def UpdatePathCov(pc,BD,z_mean,mu,c_C):
-    print(BD.shape, z_mean.shape)
-    print(pc.shape)
-    return (1-c_C)*pc + np.sqrt(c_C*(2-c_C))*np.sqrt(mu)*np.dot(BD,z_mean)
+	return (1-c_C)*pc + np.sqrt(c_C*(2-c_C))*np.sqrt(mu)*np.dot(BD,z_mean)
 
 def UpdatePathSigma(c_sigma,p_sigma,mu,B,z_mean):
 	return (1-c_sigma)*p_sigma + np.sqrt(c_sigma*(2-c_sigma)*mu)*np.dot(B,z_mean)

Analysis_and_optimization/analyze_bond_angle.py
@@ -6,6 +6,8 @@ from hoomd import deprecated
 import gsd.hoomd
 from math import pi
 import json,sys,os,glob
+import matplotlib
+matplotlib.use('Agg')
 import matplotlib.pyplot as plt
 from periodic_kdtree import PeriodicCKDTree
 
@@ -131,15 +133,17 @@ def dihedral_angle(snap,i,j,center_idx_i,center_idx_j,box):
 	loc_A_j = np.where( (bodies == j) & (type_id == type_A))[0]
 	b2 = min_image(positions[j,:]-positions[i,:],box)
 	dihedral_list = []
-	for i in range(len(loc_A_i)):
-		if (i == center_idx_i):
+	for m in range(len(loc_A_i)):
+		if (loc_A_i[m] == center_idx_i):
+			print ("rua")
 			continue
 		min_dihedral = np.inf
-		b1 = min_image(positions[i,:]-positions[loc_A_i[i],:],box)
-		for j in range(len(loc_A_j)):
-			if (j == center_idx_j):
+		b1 = min_image(positions[i,:]-positions[loc_A_i[m],:],box)
+		for n in range(len(loc_A_j)):
+			if (loc_A_j[n] == center_idx_j):
+				print ("rua")
 				continue
-			b3 = min_image(positions[loc_A_j[j],:]-positions[j,:],box)
+			b3 = min_image(positions[loc_A_j[n],:]-positions[j,:],box)
 			cur_dihedral = np.rad2deg(compute_dihedral(b1,b2,b3))
 			if (cur_dihedral <= min_dihedral):
 				min_dihedral = cur_dihedral
@@ -186,7 +190,7 @@ if __name__ == "__main__":
 	plt.title('Bond Angle')
 	plt.xlabel('Frame number')
 	plt.ylabel('Degree')
-	plt.title('mu = %.2f, epsilon = %.2f' %(dipole_params['mu'],dipole_params['eps']))
+	#plt.title('mu = %.2f, epsilon = %.2f' %(dipole_params['mu'],dipole_params['eps']))
 	f.savefig("bond_angle.pdf", bbox_inches='tight')
 
 	f = plt.figure()
@@ -194,7 +198,7 @@ if __name__ == "__main__":
 	plt.title('Dihedral Angle')
 	plt.xlabel('Frame Number')
 	plt.ylabel('Degree')
-	plt.title('mu = %.2f, epsilon = %.2f' %(dipole_params['mu'],dipole_params['eps']))
+	#plt.title('mu = %.2f, epsilon = %.2f' %(dipole_params['mu'],dipole_params['eps']))
 	f.savefig("dihedral_angle.pdf", bbox_inches='tight')
 
 	f = plt.figure()
@@ -202,7 +206,7 @@ if __name__ == "__main__":
 	plt.title('Dihedral Anlgle Frequency')
 	plt.xlabel('Degree')
 	plt.ylabel('PDF')
-	plt.title('mu = %.2f, epsilon = %.2f' %(dipole_params['mu'],dipole_params['eps']))
+	#plt.title('mu = %.2f, epsilon = %.2f' %(dipole_params['mu'],dipole_params['eps']))
 	f.savefig("dihedral_angle_freq.pdf", bbox_inches='tight')
 	plt.show()
 